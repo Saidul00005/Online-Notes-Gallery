bf44b7611af78f69aca9bf56699889847f4e0d80
Md. Sayadul Hoque <sayadul.haque@northsouth.edu>
2021-04-02 15:31:54 +0600

    #4 Uploading the Projet.

---
+++ Project/includes/footer.php
@@ -0,0 +1,7 @@
+ <footer class="foo">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p style="text-align:center; font-family: 'Monotype Corsiva'; font-size:17px;">---------Study hard------------ </p>
+                </div>
+            </div>
+   </footer>
